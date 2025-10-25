a147ac90717b77fa604178c4e9f0264af3ad8652
Aubrix <99203723+wjyhzz@users.noreply.github.com>
2025-10-25 14:19:08 +0800

    2025年10月25日14点19分

---
+++ CODE/PythonCrashCourse/Chapter02/name.py
@@ -0,0 +1,14 @@
+name="ada loVElace"
+print(name.title())#首字母大写其他小写
+print(name.upper())#全部大写
+print(name.lower())#全部小写
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"#中间有空格
+print(full_name)
+
+print(f"Hello,{full_name.upper()}!")
+
+message=f"Hello,{full_name.upper()}!"
+print(message)
