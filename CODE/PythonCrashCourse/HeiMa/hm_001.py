7cea49ee22814993a5a057824c86c57e61717b98
Aubrix <99203723+wjyhzz@users.noreply.github.com>
2025-11-21 15:10:05 +0800

    2025年11月21日15点10分

--- CODE/PythonCrashCourse/HeiMa/hm_001.py
+++ CODE/PythonCrashCourse/HeiMa/hm_001.py
@@ -2,5 +2,5 @@ name = 'xiaoming'
 age = 18
 height = 1.834567
 
-# xiaoming,18,1.834567
-print('%s,%d,%f' % (name, age, height))
+# My name is xiaoming, age 18 , 1.834567m
+print(f"My name is {name}, age {age} , {height}m")
