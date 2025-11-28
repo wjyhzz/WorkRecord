9aa83116e44eb3956e88950c81d175916a936462
Aubrix <99203723+wjyhzz@users.noreply.github.com>
2025-11-28 17:26:03 +0800

    2025年11月28日17点26分

---
+++ CODE/PythonCrashCourse/Chapter03/bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])  # trek
+print(bicycles[1])  # cannondale
+print(bicycles[3])  # specialized
+print(bicycles[-1])  # specialized
+print(bicycles[-2])  # redline
+
+message = f"我的第一辆自行车是{bicycles[0].title()}牌的"
+print(message)
---
+++ CODE/PythonCrashCourse/Chapter03/motorcycles.py
@@ -0,0 +1,14 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles[0] = 'ducati'
+
+motorcycles.append('哈雷')
+
+motorcycles.insert(0, "雅迪")
+# ['雅迪', 'ducati', 'yamaha', 'suzuki', '哈雷']
+motorcycles.remove('雅迪')
+print(motorcycles)
+
+# del motorcycles[0]
+# print(motorcycles.pop())
+# print(motorcycles.pop(0))
